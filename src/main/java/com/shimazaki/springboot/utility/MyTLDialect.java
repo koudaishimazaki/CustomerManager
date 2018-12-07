940b38e00f1de02ed577ed3da6d63d73218f94fa
k.shimazaki <k.shimazaki@wiz-net.jp>
2018-12-07 16:42:42 +0900

    fix:動作確認の為paginationファイルコメントアウト

--- src/main/java/com/shimazaki/springboot/utility/MyTLDialect.java
+++ src/main/java/com/shimazaki/springboot/utility/MyTLDialect.java
@@ -1,28 +1,29 @@
 package com.shimazaki.springboot.utility;
 
-import java.util.HashMap;
-import java.util.Map;
-
-import org.thymeleaf.context.IProcessingContext;
-import org.thymeleaf.dialect.AbstractDialect;
-import org.thymeleaf.dialect.IExpressionEnhancingDialect;
-
-public class MyTLDialect extends AbstractDialect implements IExpressionEnhancingDialect {
-
-	public MyTLDialect() {
-			super();
-		}
-
-	@Override
-	public Map<String, Object> getAdditionalExpressionObjects(IProcessingContext processingContext) {
-		Map<String, Object> objects = new HashMap<>();
-		objects.put("pageHelper", new MyTLUtility());
-		return objects;
-	}
-
-	@Override
-	public String getPrefix() {
-		return null;
-	}
-
-}
+//import java.util.HashMap;
+//import java.util.Map;
+//
+//import org.thymeleaf.context.IProcessingContext;
+//import org.thymeleaf.dialect.AbstractDialect;
+//import org.thymeleaf.dialect.IExpressionEnhancingDialect;
+//
+//public class MyTLDialect extends AbstractDialect implements IExpressionEnhancingDialect {
+//
+//	public MyTLDialect() {
+//			super();
+//		}
+//
+//	@Override
+//	public Map<String, Object> getAdditionalExpressionObjects(IProcessingContext processingContext) {
+//		Map<String, Object> objects = new HashMap<>();
+//		objects.put("pageHelper", new MyTLUtility());
+//		return objects;
+//	}
+//
+//	@Override
+//	public String getPrefix() {
+//		return null;
+//
+//	}
+//
+//}
