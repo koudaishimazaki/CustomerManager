8223ea4f1996367a89e3cc5fb6faee1ad6618978
k.shimazaki <k.shimazaki@wiz-net.jp>
2018-12-11 12:20:48 +0900

    サジェスト検索SQLの修正

--- src/main/java/com/shimazaki/springboot/repository/CustomerRepository.java
+++ src/main/java/com/shimazaki/springboot/repository/CustomerRepository.java
@@ -18,11 +18,11 @@ public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSp
 	 */
 	//姓 部分検索SQL
 	@Query(value = "SELECT first_name FROM customer WHERE first_name LIKE :first_name% LIMIT 10", nativeQuery = true)
-	public List<String> findByFirstNameLikeLimit10(@Param("firstName") String firstName);
+	public List<String> findByFirstNameLikeLimit10(@Param("first_name") String firstName);
 
 	//名 部分検索SQL
 	@Query(value = "SELECT last_name FROM customer WHERE last_name LIKE :last_name% LIMIT 10", nativeQuery = true)
-	public List<String> findByLastNameLikeLimit10(@Param("lastName") String lastName);
+	public List<String> findByLastNameLikeLimit10(@Param("last_name") String lastName);
 
 	//姓(かな) 部分検索SQL
 	@Query(value = "SELECT f_name_kana FROM customer WHERE f_name_kana LIKE :f_name_kana% LIMIT 10", nativeQuery = true)
