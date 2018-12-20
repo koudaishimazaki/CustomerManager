1f56adea10fecdfb0bee99688d1b9f7dfe90b1da
k.shimazaki <k.shimazaki@wiz-net.jp>
2018-12-20 10:44:40 +0900

    fix:サジェストの表示をユニーク化

--- src/main/java/com/shimazaki/springboot/repository/CustomerRepository.java
+++ src/main/java/com/shimazaki/springboot/repository/CustomerRepository.java
@@ -17,19 +17,19 @@ public interface CustomerRepository extends JpaRepository<Customer, Long>, JpaSp
 	 * 検索フィールドサジェスト用SQL
 	 */
 	//姓 部分検索SQL
-	@Query(value = "SELECT first_name FROM customer WHERE first_name LIKE :first_name% LIMIT 10", nativeQuery = true)
+	@Query(value = "SELECT DISTINCT first_name FROM customer WHERE first_name LIKE :first_name% LIMIT 10", nativeQuery = true)
 	public List<String> findByFirstNameLikeLimit10(@Param("first_name") String firstName);
 
 	//名 部分検索SQL
-	@Query(value = "SELECT last_name FROM customer WHERE last_name LIKE :last_name% LIMIT 10", nativeQuery = true)
+	@Query(value = "SELECT DISTINCT last_name FROM customer WHERE last_name LIKE :last_name% LIMIT 10", nativeQuery = true)
 	public List<String> findByLastNameLikeLimit10(@Param("last_name") String lastName);
 
 	//姓(かな) 部分検索SQL
-	@Query(value = "SELECT f_name_kana FROM customer WHERE f_name_kana LIKE :f_name_kana% LIMIT 10", nativeQuery = true)
+	@Query(value = "SELECT DISTINCT f_name_kana FROM customer WHERE f_name_kana LIKE :f_name_kana% LIMIT 10", nativeQuery = true)
 	public List<String> findByFirstNameKanaLikeLimit10(@Param("f_name_kana") String firstNameKana);
 
 	//姓(かな) 部分検索SQL
-	@Query(value = "SELECT l_name_kana FROM customer WHERE l_name_kana LIKE :l_name_kana% LIMIT 10", nativeQuery = true)
+	@Query(value = "SELECT DISTINCT l_name_kana FROM customer WHERE l_name_kana LIKE :l_name_kana% LIMIT 10", nativeQuery = true)
 	public List<String> findByLastNameKanaLikeLimit10(@Param("l_name_kana") String lastNameKana);
 
 
